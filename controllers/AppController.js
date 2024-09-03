31b1f44f7e9fd0051e2e50419b614da019ccfb3e
godfreydekew <dekewgodfrey@gmail.com>
2024-09-03 13:41:20 +0300

    Task 2

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -6,8 +6,10 @@ class AppController {
     res.status(200).json({ redis: redisClient.isAlive(), db: dbClient.isAlive() });
   }
 
-  static getStats(req, res) {
-    res.status(200).json({ users: dbClient.nbUsers(), files: dbClient.nbFiles() });
+  static async getStats(req, res) {
+    const usersCount = await dbClient.nbUsers();
+    const filesCount = await dbClient.nbFiles();
+    res.status(200).json({ users: usersCount, files: filesCount });
   }
 }
 
